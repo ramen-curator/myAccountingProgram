e3837d5c89515d39b640d68ccd1db027d71371de
赖铭志 <ramen.curator@gmail.com>
2021-11-25 02:31:11 +0800

    refactor: 优化getDateRange函数代码结构

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,26 @@
 export const getObjSum = (o: { [name: string]: number }) =>
   Object.keys(o).reduce((s, k) => s + o[k], 0);
 
+type month = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 interface monthObj {
   y: number;
-  m: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
+  m: month;
 }
-const getAYearMonth = (y: number): monthObj => {
-  // @ts-ignore 'fill' does not exist on type 'any[]'
-  return new Array(12).fill(" ").map((_, m) => ({ y, m: m + 1 }));
+
+const initMonthsInAYear = (y: number, sm: month, em: month) => {
+  const result: monthObj[] = [];
+  for (let m = sm; m <= em; m++) {
+    result.push({ y, m });
+  }
+  return result;
+};
+
+const initIntRange = (s: number, e: number) => {
+  const result: number[] = [];
+  for (let i = s; i <= e; i++) {
+    result.push(i);
+  }
+  return result;
 };
 
 //获取日期范围
@@ -17,26 +30,18 @@ export const getDateRange = (startDate: monthObj, endDate: monthObj) => {
   if (diffYear === 0) {
     const diffMonth = endDate.m - startDate.m; // 相差几月
     if (diffMonth < 0) throw new Error("结束日期应大于开始日期");
-    const result: monthObj[] = [];
-    for (let m = startDate.m; m <= endDate.m; m++) {
-      result.push({ y: startDate.y, m });
-    }
-    return result;
+    return initMonthsInAYear(startDate.y, startDate.m, endDate.m);
   }
   if (diffYear > 0) {
-    let startYearDate: monthObj[] = [];
-    let endYearDate: monthObj[] = [];
-    let years: monthObj[] = [];
-    for (let m = startDate.m; m <= 12; m++) {
-      startYearDate.push({ y: startDate.y, m });
-    }
-    for (let m = 1; m <= endDate.m; m++) {
-      // @ts-ignore Type 'number' is not assignable to type '1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12'.ts(2322)
-      endYearDate.push({ y: endDate.y, m });
-    }
-    for (let y = startDate.y + 1; y < endDate.y; y++) {
-      years = years.concat(getAYearMonth(y));
-    }
+    const startYearDate: monthObj[] = initMonthsInAYear(
+      startDate.y,
+      startDate.m,
+      12
+    );
+    const endYearDate: monthObj[] = initMonthsInAYear(endDate.y, 1, endDate.m);
+    const years: monthObj[] = initIntRange(startDate.y + 1, endDate.y)
+      .map((y) => initMonthsInAYear(y, 1, 12))
+      .flat(1);
     return [...startYearDate, ...years, ...endYearDate];
   }
 };
