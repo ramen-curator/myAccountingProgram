5ae5942146a14ddde966f155d0e7772bbb021a2c
赖铭志 <ramen.curator@gmail.com>
2021-11-25 02:36:48 +0800

    refactor: 改善变量名字

--- src/util.ts
+++ src/util.ts
@@ -33,7 +33,7 @@ export const getDateRange = (startDate: monthObj, endDate: monthObj) => {
   if (diffYear === 0)
     return initMonthsInAYear(startDate.y, startDate.m, endDate.m);
   if (diffYear > 0) {
-    const startYearDate: monthObj[] = initMonthsInAYear(
+    const startYearMonth: monthObj[] = initMonthsInAYear(
       startDate.y,
       startDate.m,
       12
@@ -41,8 +41,8 @@ export const getDateRange = (startDate: monthObj, endDate: monthObj) => {
     const years: monthObj[] = initIntRange(startDate.y + 1, endDate.y)
       .map((y) => initMonthsInAYear(y, 1, 12))
       .flat(1);
-    const endYearDate: monthObj[] = initMonthsInAYear(endDate.y, 1, endDate.m);
+    const endYearMonth: monthObj[] = initMonthsInAYear(endDate.y, 1, endDate.m);
 
-    return [...startYearDate, ...years, ...endYearDate];
+    return [...startYearMonth, ...years, ...endYearMonth];
   }
 };
