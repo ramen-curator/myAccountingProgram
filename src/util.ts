3ff084d126b44c922ea6b80ff8e8dc05eff279fe
赖铭志 <ramen.curator@gmail.com>
2021-11-25 02:10:00 +0800

    refactor: getYearMonth提取到getDateRange外

--- src/util.ts
+++ src/util.ts
@@ -5,13 +5,14 @@ interface monthObj {
   y: number;
   m: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
+const getAYearMonth = (y: number): monthObj => {
+  // @ts-ignore 'fill' does not exist on type 'any[]'
+  return new Array(12).fill(" ").map((_, m) => ({ y, m: m + 1 }));
+};
+
 //获取日期范围
 export const getDateRange = (startDate: monthObj, endDate: monthObj) => {
-  const getAYearDate = (y: number): monthObj => {
-    // @ts-ignore 'fill' does not exist on type 'any[]'
-    return new Array(12).fill(" ").map((_, m) => ({ y, m: m + 1 }));
-  };
-  const diffYear = endDate.y - startDate.y;
+  const diffYear = endDate.y - startDate.y; // 相差几年
   const diffMonth = endDate.m - startDate.m;
   if (diffYear < 0) {
     throw new Error("日期不对1");
@@ -36,7 +37,7 @@ export const getDateRange = (startDate: monthObj, endDate: monthObj) => {
       endYearDate.push({ y: endDate.y, m });
     }
     for (let y = startDate.y + 1; y < endDate.y; y++) {
-      years = years.concat(getAYearDate(y));
+      years = years.concat(getAYearMonth(y));
     }
     return [...startYearDate, ...years, ...endYearDate];
   }
