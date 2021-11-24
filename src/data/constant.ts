57a1aeb808f0f865f6261ba83371188093fc8245
赖铭志 <ramen.curator@gmail.com>
2021-11-25 01:38:39 +0800

    refactor: 抽象出 MyWage 类

--- src/data/constant.ts
+++ src/data/constant.ts
@@ -13,20 +13,30 @@ export const lowestLife = {
   给妈妈: 1500,
 };
 
-// 获取医社保占的比例
-const getInsuranceRate = (wage: number, insurance: number) =>
-  Number((insurance / wage).toFixed(2));
-
+class MyWage {
+  nowWage = 1000; // 目前工资
+  now医保 = 100;
+  nowCwage = this.getComingWage(this.nowWage); // 到手工资
+  医保率 = this.getInsuranceRate(this.nowWage, this.now医保);
+  willWage = 18000; // 转正后工资
+  will医保 = this.getInsurance(this.willWage, this.医保率);
+  willCWage = this.willWage * (1 - this.医保率); // 转正后到手工资
+  getInsuranceRate(wage: number, insurance: number) {
+    // 获取医社保占的比例
+    return Number((insurance / wage).toFixed(2));
+  }
+  getComingWage(wage: number) {
+    // 获取到手工资
+    return wage * (1 - this.医保率);
+  }
+  getInsurance(wage: number, insuranceRate: number) {
+    // 获取医保费用
+    return wage * insuranceRate;
+  }
+}
 // 到手工资 · 不变化版
-const getWillCWage = () => {
-  const nowWage = 9000;
-  const now医保 = 100;
-  const 医保率 = getInsuranceRate(nowWage, now医保);
-  const willWage = 18000;
-  const willCWage = willWage * (1 - 医保率);
-  return willCWage;
-};
+export const willCWage = new MyWage().willCWage;
 
 // 到手工资 · 写在 工资.ts 里的
-export const getYXWillCWage = (y, m) => getValue(ta的工资,y,m);
+export const getYXWillCWage = (y, m) => getValue(ta的工资, y, m);
 // export const willCWage = getWillCWage();
