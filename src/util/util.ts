178ed55dcb0cd5e0a47cb0265508d9470ba8003a
赖铭志 <ramen.curator@gmail.com>
2021-11-25 01:55:59 +0800

    refactor: 变量dateObj改名成monthObj

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,13 +1,13 @@
 export const getObjSum = (o: { [name: string]: number }) =>
   Object.keys(o).reduce((s, k) => s + o[k], 0);
 
-interface dateObj {
+interface monthObj {
   y: number;
   m: 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 }
 //获取日期范围
-export const getDateRange = (startDate: dateObj, endDate: dateObj) => {
-  const getAYearDate = (y: number): dateObj => {
+export const getDateRange = (startDate: monthObj, endDate: monthObj) => {
+  const getAYearDate = (y: number): monthObj => {
     // @ts-ignore 'fill' does not exist on type 'any[]'
     return new Array(12).fill(" ").map((_, m) => ({ y, m: m + 1 }));
   };
@@ -18,16 +18,16 @@ export const getDateRange = (startDate: dateObj, endDate: dateObj) => {
   }
   if (diffYear === 0) {
     if (diffMonth < 0) throw new Error("日期不对2");
-    let result: dateObj[] = [];
+    let result: monthObj[] = [];
     for (let m = startDate.m; m <= endDate.m; m++) {
       result.push({ y: startDate.y, m });
     }
     return result;
   }
   if (diffYear > 0) {
-    let startYearDate: dateObj[] = [];
-    let endYearDate: dateObj[] = [];
-    let years: dateObj[] = [];
+    let startYearDate: monthObj[] = [];
+    let endYearDate: monthObj[] = [];
+    let years: monthObj[] = [];
     for (let m = startDate.m; m <= 12; m++) {
       startYearDate.push({ y: startDate.y, m });
     }
