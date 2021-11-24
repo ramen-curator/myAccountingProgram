b7796051c2ed7abcef02f29eb8a18e76f65214e7
赖铭志 <ramen.curator@gmail.com>
2021-11-25 00:50:46 +0800

    refactor: 抽象 获取医社保比率 函数

--- src/data/constant.ts
+++ src/data/constant.ts
@@ -9,15 +9,19 @@ export const lowestLife = {
   吃饭: 1000,
   话费: 100,
   VPN: 0,
-  零花钱:200,
+  零花钱: 200,
   给妈妈: 1500,
 };
+
+// 获取医社保占的比例
+const getInsuranceRate = (wage: number, insurance: number) =>
+  Number((insurance / wage).toFixed(2));
 
 // 到手工资 · 不变化版
 const getWillCWage = () => {
   const nowWage = 9000;
   const now医保 = 100;
-  const 医保率 = Number((now医保 / nowWage).toFixed(2));
+  const 医保率 = getInsuranceRate(nowWage, now医保);
   const willWage = 18000;
   const willCWage = willWage * (1 - 医保率);
   return willCWage;
