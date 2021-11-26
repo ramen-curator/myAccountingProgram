4d71fb597806fe3e2ec72e481e81214934413182
赖铭志 <ramen.curator@gmail.com>
2021-11-26 21:54:20 +0800

    style: 格式化代码

--- src/data/贷款/京东白条.ts
+++ src/data/贷款/京东白条.ts
@@ -1,16 +1,14 @@
 export default [
-    
-    { year: 2021, month: 12, value: 541.63},
-    { year: 2022, month: 1, value: 541.63 },
-    { year: 2022, month: 2, value:541.63 },
-    { year: 2022, month: 3, value:541.63},
-    { year: 2022, month: 4, value: 541.63 },
-    { year: 2022, month: 5, value: 541.63},
-    { year: 2022, month: 6, value: 541.63 },
-    { year: 2022, month: 7, value: 541.63 },
-    { year: 2022, month: 8, value:541.63 },
-    { year: 2022, month: 9, value:541.63 }, 
-    { year: 2022, month:10, value:541.63 },
-    { year: 2022, month:11, value:541.63 },
-  ];
-  
+  { year: 2021, month: 12, value: 541.63 },
+  { year: 2022, month: 1, value: 541.63 },
+  { year: 2022, month: 2, value: 541.63 },
+  { year: 2022, month: 3, value: 541.63 },
+  { year: 2022, month: 4, value: 541.63 },
+  { year: 2022, month: 5, value: 541.63 },
+  { year: 2022, month: 6, value: 541.63 },
+  { year: 2022, month: 7, value: 541.63 },
+  { year: 2022, month: 8, value: 541.63 },
+  { year: 2022, month: 9, value: 541.63 },
+  { year: 2022, month: 10, value: 541.63 },
+  { year: 2022, month: 11, value: 541.63 },
+];
