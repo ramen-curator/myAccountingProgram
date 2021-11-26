e4a6bfc45932d3f0744c5d7ea07bef484de42d6e
赖铭志 <ramen.curator@gmail.com>
2021-11-26 21:09:00 +0800

    refactor: 获取每月剩余金额 移动到compute

--- src/compute.ts
+++ src/compute.ts
@@ -1,6 +1,8 @@
-import { homeRent } from "./data/constant";
+import { homeRent, lowestLife as 最低生活水平 } from "./data/constant";
+import 工资_monthlyList from "./data/工资";
 import 贷款 from "./data/贷款";
 import { getValue } from "./helper";
+import { getObjSum, getDateRange } from "./util";
 
 // 合租后的房租
 export const getHomeRentAfterSharingBy = (peopleNum: number) =>
@@ -10,3 +12,25 @@ export const getHomeRentAfterSharingBy = (peopleNum: number) =>
 export const getLoanValue = (y, m) => {
   return 贷款.reduce((s, o) => s + getValue(o, y, m), 0);
 };
+
+// 获取每月剩钱，是工资-最低生活水平-贷款-每月买点啥
+// 如果写了第三个参数，就每个月按第三个数算
+// 如果没写第三个参数，每个月工资就按“/data/工资”文件那样地算
+export const getEveryMonthRest = (
+  dateRange,
+  needPay: number[],
+  wage?: number
+) =>
+  dateRange.map(({ y, m }) => {
+    const theWage = wage ? wage : getValue(工资_monthlyList, y, m);
+    return {
+      year: y,
+      month: m,
+      value: (
+        theWage -
+        getObjSum(最低生活水平) -
+        getLoanValue(y, m) -
+        needPay.reduce((s, v) => s + v, 0)
+      ).toFixed(2),
+    };
+  });
--- src/main.ts
+++ src/main.ts
@@ -1,40 +1,28 @@
-import { lowestLife as 最低生活水平, willCWage } from "./data/constant";
-import { getObjSum, getDateRange } from "./util";
-import { getHomeRentAfterSharingBy, getLoanValue } from "./compute";
-import 工资_monthlyList from "./data/工资";
-import { getValue } from "./helper";
+import { getDateRange } from "./util";
+import {
+  getHomeRentAfterSharingBy,
+  getLoanValue,
+  getEveryMonthRest,
+} from "./compute";
 
 // 开始年份月份，结束年份月份
 const dateRange = getDateRange("2021/12 ~ 2023/3");
 
-// 获取每月剩钱，
-const getEveryMonthRest = (dateRange, needPay: number[], wage?: number) =>
-  dateRange.map(({ y, m }) => {
-    const theWage = wage ? wage : getValue(工资_monthlyList, y, m);
-    return {
-      year: y,
-      month: m,
-      value: (
-        theWage -
-        getObjSum(最低生活水平) -
-        getLoanValue(y, m) -
-        needPay.reduce((s, v) => s + v, 0)
-      ).toFixed(2),
-    };
-  });
-
 const needPay = [];
-needPay.push(getHomeRentAfterSharingBy(2));
-console.log("每个月剩钱", getEveryMonthRest(dateRange,needPay));
+needPay.push(getHomeRentAfterSharingBy(2));// 应付房租
+console.log("每个月剩钱", getEveryMonthRest(dateRange, needPay));
 
 const 贷款总额 = dateRange.reduce((s, { y, m }) => {
   return s + getLoanValue(y, m);
 }, 0);
 console.log("贷款总额", 贷款总额.toFixed(2));
 
-const 余钱总额 = getEveryMonthRest(dateRange,needPay).reduce((s, { value }) => {
-  return s + Number(value);
-}, 0);
+const 余钱总额 = getEveryMonthRest(dateRange, needPay).reduce(
+  (s, { value }) => {
+    return s + Number(value);
+  },
+  0
+);
 console.log("余钱总额", 余钱总额.toFixed(2));
 
 // todo 一年挣多少钱
